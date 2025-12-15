393c271704b68c16db717abfd6e496785cbdc320
cputnam-a11y <unknown@cputnam-a11y.github.io>
2025-12-15 16:45:04 -0600

    Tooltips, it is.

--- src/client/java/io/github/cputnama11y/anitpothicspawners/impl/AntipothicSpawnersClient.java
+++ src/client/java/io/github/cputnama11y/anitpothicspawners/impl/AntipothicSpawnersClient.java
@@ -1,10 +1,38 @@
 package io.github.cputnama11y.anitpothicspawners.impl;
 
+import io.github.cputnama11y.antipothicspawners.impl.AntipothicSpawners;
+import io.github.cputnama11y.antipothicspawners.impl.stats.SpawnerStats;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.TypedEntityData;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class AntipothicSpawnersClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        ItemTooltipCallback.EVENT.register((stack, context, tooltipType, lines) -> {
+            if (!stack.is(Items.SPAWNER) || !stack.has(DataComponents.BLOCK_ENTITY_DATA) || context.registries() == null)
+                return;
+
+            if (!Minecraft.getInstance().hasShiftDown()) {
+                lines.add(AntipothicSpawners.lang("misc", "shift_stats").withStyle(ChatFormatting.GRAY));
+                return;
+            }
+            TypedEntityData<@NotNull BlockEntityType<?>> data = stack.get(DataComponents.BLOCK_ENTITY_DATA);
+            if (data == null) return;
+            SpawnerBlockEntity spawner = new SpawnerBlockEntity(BlockPos.ZERO, Blocks.SPAWNER.defaultBlockState());
+            data.loadInto(spawner, Objects.requireNonNull(context.registries()));
+            SpawnerStats.generateTooltip(spawner, lines::add);
+        });
     }
 }
